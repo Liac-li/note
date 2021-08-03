6c78074ee860ee38ea0f500915af58d9c6166a30
leelin-arch <lyt-42@outlook.com>
2021-08-04 00:55:07 +0800

    Update: 2021 Aug 04

--- push.sh
+++ push.sh
@@ -1,5 +1,16 @@
 #/bin/bash
 
-git add .
-git commit -m "echo $(date) | awk '{print $7" "$2" "$3}'" --allow-empty
+DATE_msg="`date +'%Y %b %d'`"
+Add_msg=$1
+echo $DATE_msg
+
+git add ./
+
+if [ -z "$Add_msg" ]; then
+	git commit -m "Update: $DATE_msg" --allow-empty
+else
+	git commit -m "Update: $DATE_msg -- $Add_msg" --allow-empty
+fi
+
 git push origin master
+
